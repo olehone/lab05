d67836c8da08353d9885717e7ffa983d619737a0
olehvolodko <oleh.volodko.ri.2022@lpnu.ua>
2022-11-03 22:11:34 +0200

    second test

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = floor(k(1, 2) * 10000000) / 10000000;
+			Assert::AreEqual(t, 0.0295785);
+		}
+	};
+}
