686c59d8ad7ea956d011f012ff548d3a447b382f
olehvolodko <oleh.volodko.ri.2022@lpnu.ua>
2022-11-04 07:18:45 +0200

    lab5.2

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = floor (A(1.5, 2, 2) * 10000000) / 10000000;
+			Assert::AreEqual(t, 2.25);
+		}
+	};
+}
